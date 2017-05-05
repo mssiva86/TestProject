8c89fbf1368c16d517f97c1f08f99a1d9da5c5bf
Vidhyaa Saravanan <Vidhyaa Saravanan>
2017-05-04 21:19:18 -0500

    changes on 5/42017 home

--- src/pages/people/people.ts
+++ src/pages/people/people.ts
@@ -14,6 +14,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class People {
 
+
+  users = [
+    {  id : 1, name : 'Siva' , age : 24 , city : 'New York' ,  country : 'United States'  },
+    {  id : 1, name : 'Sakthi' , age : 24 , city : 'Los Vagas' , country : 'United States'  },
+    {  id : 1, name : 'SSS' , age : 24 , city : 'Texas' , country : 'United States'  },
+    {  id : 1, name : 'Vidhyaa' , age : 24 , city : 'Alaska' , country : 'United States'  },
+    {  id : 1, name : 'Elango', age : 24 , city : 'Alabama' , country : 'United States'  },
+    {  id : 1, name : 'Santhana' , age : 24 , city : 'Chennai' , country : 'India'  },
+    {  id : 1, name : 'Krishnan' , age : 24 , city : 'Tirunelveli' , country : 'India' },
+    {  id : 1, name : 'Sadhana' , age : 24 , city : 'Delhi' , country :'India' },
+    {  id : 1, name : 'Vishnu' , age : 24 , city : 'Mumbai' , country : 'India'  },
+    {  id : 1, name : 'Priya', age : 24 , city : 'Bengal' , country : 'India'  },
+  ]
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
